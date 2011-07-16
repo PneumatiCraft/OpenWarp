4da700e954c04bbb50933e5982bef6d3835ef210
Tim Ekl <lithium3141@gmail.com>
2011-07-16 14:22:17 -0600

    Start warp set type check; need plugin-wide private warp support

--- src/com/lithium3141/OpenWarp/commands/OWWarpSetCommand.java
+++ src/com/lithium3141/OpenWarp/commands/OWWarpSetCommand.java
@@ -2,6 +2,7 @@ package com.lithium3141.OpenWarp.commands;
 
 import java.util.List;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -24,16 +25,32 @@ public class OWWarpSetCommand extends OWCommand {
     public boolean execute(CommandSender sender, Command command, String commandLabel, List<String> args) {
         if(!this.checkPlayerSender(sender)) return true;
         
+        // Grab player info
         CraftPlayer player = (CraftPlayer)sender;
         Location playerLoc = player.getLocation();
         
         // TODO quota check
         
-        // TODO warp type check
+        // Find warp type
+        String warpType;
+        if(args.size() >= 2) {
+            warpType = args.get(1);
+        } else {
+            warpType = "public";
+        }
         
+        if(!warpType.equals("public") && !warpType.equals("private")) {
+            player.sendMessage(ChatColor.YELLOW + "Usage: /warp set {NAME} [public|private]");
+        }
+        
+        // Create and set warp
         Warp warp = new Warp(this.plugin, args.get(0), playerLoc);
+        if(warpType.equals("public")) {
+            this.plugin.getPublicWarps().put(warp.getName(), warp);
+        } else if(warpType.equals("private")) {
+            // TODO save private warp
+        }
         
-        this.plugin.getPublicWarps().put(warp.getName(), warp);
         
         return true;
     }
