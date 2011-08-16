7cfb62f7b602a4faee1dde83e1d8ef201b7c0c53
Tim Ekl <lithium3141@gmail.com>
2011-08-15 23:10:11 -0500

    Refactor location tracker for string keys, multiple ignores

--- src/com/lithium3141/OpenWarp/OWLocationTracker.java
+++ src/com/lithium3141/OpenWarp/OWLocationTracker.java
@@ -8,38 +8,82 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 public class OWLocationTracker {
-    private Map<Player, Location> previousLocations = new HashMap<Player, Location>();
-    private Map<Player, Stack<Location>> locationStacks = new HashMap<Player, Stack<Location>>();
-    private Map<Player, Boolean> ignoreNexts = new HashMap<Player, Boolean>();
+    private Map<String, Location> previousLocations = new HashMap<String, Location>();
+    private Map<String, Stack<Location>> locationStacks = new HashMap<String, Stack<Location>>();
+    private Map<String, Integer> ignoreNexts = new HashMap<String, Integer>();
+    
+    private OpenWarp plugin;
+    
+    public OWLocationTracker(OpenWarp plugin) {
+        this.plugin = plugin;
+    }
     
     public Location getPreviousLocation(Player player) {
-        Location loc = this.previousLocations.get(player); 
+        return this.getPreviousLocation(player.getName());
+    }
+    
+    public Location getPreviousLocation(String playerName) {
+        Location loc = this.previousLocations.get(playerName); 
         if(loc != null) {
             return loc;
         } else {
-            return player.getLocation();
+            return null;
         }
     }
     
     public void setPreviousLocation(Player player, Location location) {
-        if(!(this.ignoreNexts.containsKey(player) && this.ignoreNexts.get(player) == true)) {
-            this.previousLocations.put(player, location);
+        this.setPreviousLocation(player.getName(), location);
+    }
+    
+    public void setPreviousLocation(String playerName, Location location) {
+        if(!(this.ignoreNexts.containsKey(playerName) && this.ignoreNexts.get(playerName) > 0)) {
+            System.out.println("Setting previous location (" + playerName + "): " + location);
+            this.previousLocations.put(playerName, location);
         }
-        this.ignoreNexts.put(player, false);
+        if(this.ignoreNexts.containsKey(playerName)) {
+            this.ignoreNexts.put(playerName, this.ignoreNexts.get(playerName) - 1);            
+        } else {
+            this.ignoreNexts.put(playerName, 0);
+        }
+        this.plugin.saveConfigurations(playerName);
     }
     
     public void clearPreviousLocation(Player player) {
-        this.previousLocations.remove(player);
+        this.clearPreviousLocation(player.getName());
+    }
+    
+    public void clearPreviousLocation(String playerName) {
+        this.previousLocations.remove(playerName);
     }
     
     public void ignoreNextSet(Player player) {
-        this.ignoreNexts.put(player, true);
+        this.ignoreNextSet(player.getName());
+    }
+    
+    public void ignoreNextSet(String playerName) {
+        if(this.ignoreNexts.containsKey(playerName)) {
+            this.ignoreNexts.put(playerName, this.ignoreNexts.get(playerName) - 1);
+        } else {
+            this.ignoreNexts.put(playerName, 1);
+        }
+    }
+    
+    public void ignoreNextSets(Player player, int count) {
+        this.ignoreNextSets(player.getName(), count);
+    }
+    
+    public void ignoreNextSets(String playerName, int count) {
+        this.ignoreNexts.put(playerName, count);
     }
     
     public Stack<Location> getLocationStack(Player player) {
-        if(this.locationStacks.get(player) == null) {
-            this.locationStacks.put(player, new Stack<Location>());
+        return this.getLocationStack(player.getName());
+    }
+    
+    public Stack<Location> getLocationStack(String playerName) {
+        if(this.locationStacks.get(playerName) == null) {
+            this.locationStacks.put(playerName, new Stack<Location>());
         }
-        return this.locationStacks.get(player);
+        return this.locationStacks.get(playerName);
     }
 }
