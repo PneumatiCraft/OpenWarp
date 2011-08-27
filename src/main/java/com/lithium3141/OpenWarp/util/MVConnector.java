e285c78725fab450ab5da5f0aa405976f862e41f
Eric Stokes <fernferret@gmail.com>
2011-08-26 18:49:54 -0600

    Add Initial MVConnector. Does Nothing.

---
+++ src/main/java/com/lithium3141/OpenWarp/util/MVConnector.java
@@ -0,0 +1,15 @@
+package com.lithium3141.OpenWarp.util;
+
+import org.bukkit.plugin.Plugin;
+
+import com.onarandombox.MultiverseCore.MultiverseCore;
+
+public class MVConnector {
+    private MultiverseCore core;
+
+    public MVConnector(Plugin plugin) {
+        if(plugin != null) {
+            this.core = (MultiverseCore) plugin;
+        }
+    }
+}
