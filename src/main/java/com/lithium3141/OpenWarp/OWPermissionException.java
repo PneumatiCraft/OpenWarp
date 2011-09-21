83c8a4942368464e5392fd3b3ae0464e2d685f02
Tim Ekl <lithium3141@gmail.com>
2011-09-21 11:43:20 -0400

    Document OWPermissionException

--- src/main/java/com/lithium3141/OpenWarp/OWPermissionException.java
+++ src/main/java/com/lithium3141/OpenWarp/OWPermissionException.java
@@ -1,10 +1,21 @@
 package com.lithium3141.OpenWarp;
 
+/**
+ * Exception thrown when encountering a permissions-based error.
+ */
 public class OWPermissionException extends Exception {
+    /**
+     * Create a new OWPermissionException.
+     */
     public OWPermissionException() {
         super();
     }
     
+    /**
+     * Create a new OWPermissionException with the given message.
+     *
+     * @param message The message to include in this OWPermissionException.
+     */
     public OWPermissionException(String message) {
         super(message);
     }
