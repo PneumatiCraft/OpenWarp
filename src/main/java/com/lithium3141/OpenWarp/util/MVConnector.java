89a4b505a0fd5aec806e80a86a3c6d06bafc0329
Tim Ekl <lithium3141@gmail.com>
2011-09-21 19:52:32 -0400

    Document MVConnector

--- src/main/java/com/lithium3141/OpenWarp/util/MVConnector.java
+++ src/main/java/com/lithium3141/OpenWarp/util/MVConnector.java
@@ -4,9 +4,18 @@ import org.bukkit.plugin.Plugin;
 
 import com.onarandombox.MultiverseCore.MultiverseCore;
 
+/**
+ * Utility class for interfacing with Multiverse 2. Required stub
+ * for the necessary interfaces.
+ */
 public class MVConnector {
     private MultiverseCore core;
 
+    /**
+     * Create a new MVConnector backed by the given Plugin.
+     *
+     * @param plugin The Plugin instance for which this connector is operating.
+     */
     public MVConnector(Plugin plugin) {
         if(plugin != null) {
             this.core = (MultiverseCore) plugin;
