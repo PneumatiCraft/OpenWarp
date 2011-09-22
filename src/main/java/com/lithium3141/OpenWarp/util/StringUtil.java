ac5548b28d98d34ae7441cb92d70b217ad474ef7
Tim Ekl <lithium3141@gmail.com>
2011-09-21 20:01:28 -0400

    Document StringUtil

--- src/main/java/com/lithium3141/OpenWarp/util/StringUtil.java
+++ src/main/java/com/lithium3141/OpenWarp/util/StringUtil.java
@@ -3,7 +3,19 @@ package com.lithium3141.OpenWarp.util;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Utility class for various string operations. Primarily operates with
+ * String arrays.
+ */
 public class StringUtil {
+    /**
+     * Check whether an array of String objects contains another String.
+     *
+     * @param arr The String array to search.
+     * @param x The String to search for.
+     * @return True if any element in <code>arr</code> is equal to <code>x</code>
+     * using the String <code>.equals()</code> method.
+     */
     public static boolean arrayContains(String[] arr, String x) {
         for(int i = 0; i < arr.length; i++) {
             if(arr[i].equals(x)) {
@@ -13,6 +25,16 @@ public class StringUtil {
         return false;
     }
     
+    /**
+     * Trim the contents of one String array from the beginning of another. Iteratively
+     * removes elements in <code>trim</code> from the array <code>value</code>, stopping
+     * when <code>trim</code> is completely iterated or no longer matches <code>value</code>.
+     *
+     * @param value The String array to trim.
+     * @param trim The String values to remove.
+     * @return A new String array containing the contents of <code>value</code> not also
+     * contained in <code>trim</code>.
+     */
     public static String[] trimArrayLeft(String[] value, String[] trim) {
         int idx = 0;
         for(idx = 0; idx < Math.min(value.length, trim.length); idx++) {
@@ -29,6 +51,14 @@ public class StringUtil {
         return result;
     }
     
+    /**
+     * Join an array of Strings into a single String, separated by a given delimiter.
+     *
+     * @param array The array of Strings to join.
+     * @param delim The String to intersperse in <code>array</code> when joining.
+     * @return A single String containing each element in <code>array</code>, separated
+     * by <code>delim</code>.
+     */
     public static String arrayJoin(String[] array, String delim) {
         if(array.length == 0) {
             return "";
@@ -42,10 +72,28 @@ public class StringUtil {
         return result;
     }
     
+    /**
+     * Join an array of Strings into a single String, separated by a space.
+     *
+     * @param array The array of Strings to join.
+     * @return A single String containing each element in <code>array</code>, separated
+     * by a single space.
+     * @see #arrayJoin(String[], String)
+     */
     public static String arrayJoin(String[] array) {
         return arrayJoin(array, " ");
     }
     
+    /**
+     * Trim the contents of one String List from the beginning of another. Iteratively
+     * removes elements in <code>trim</code> from the array <code>value</code>, stopping
+     * when <code>trim</code> is completely iterated or no longer matches <code>value</code>.
+     *
+     * @param value The String List to trim.
+     * @param trim The String values to remove.
+     * @return A new String List containing the contents of <code>value</code> not also
+     * contained in <code>trim</code>.
+     */
     public static List<String> trimListLeft(List<String> value, List<String> trim) {
         List<String> result = new ArrayList<String>();
         
